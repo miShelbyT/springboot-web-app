e6e07690a477400beeafd0880397346de8ab9a72
Justin <yibbidy@gmail.com>
2025-05-21 00:32:48 -0400

    put back

--- src/main/java/com/talbert/inventorymgmt/InventorymgmtApplication.java
+++ src/main/java/com/talbert/inventorymgmt/InventorymgmtApplication.java
@@ -9,11 +9,7 @@ public class InventorymgmtApplication {
 
 	public static void main(String[] args) { 
 		try {
-			//SpringApplication.run(InventorymgmtApplication.class, args); 
-			 System.out.println("Main method sleeping...");
-			Thread.sleep(60000);
-			System.out.println("Done sleeping.");
-
+			SpringApplication.run(InventorymgmtApplication.class, args); 
 			System.out.println("Application has started and SpringApplication.run completed.");
 		} catch( Exception e) {
 			System.out.println("Exception caught. Application crashed:");
