d850695f40b59d82e8ad0b2206aa75720cbb73c3
Justin <yibbidy@gmail.com>
2025-05-21 00:23:51 -0400

    sleep test

--- src/main/java/com/talbert/inventorymgmt/InventorymgmtApplication.java
+++ src/main/java/com/talbert/inventorymgmt/InventorymgmtApplication.java
@@ -9,7 +9,11 @@ public class InventorymgmtApplication {
 
 	public static void main(String[] args) { 
 		try {
-			SpringApplication.run(InventorymgmtApplication.class, args); 
+			//SpringApplication.run(InventorymgmtApplication.class, args); 
+			 System.out.println("Main method sleeping...");
+			Thread.sleep(60000);
+			System.out.println("Done sleeping.");
+
 			System.out.println("Application has started and SpringApplication.run completed.");
 		} catch( Exception e) {
 			System.out.println("Exception caught. Application crashed:");
